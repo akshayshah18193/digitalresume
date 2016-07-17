0970fda5de5b604cc79432b9e752add1f0eaa730
akshayshah18193 <akshayshah.1893@gmail.com>
2016-07-18 02:42:53 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="main.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+
+     <div  class="container-fluid bg-1 text-center">
+  <h1 class="margin"> I am </h1>
+  <img src="akshay.jpg" class="img-responsive img-circle margin" style="display:inline" alt="Profile Pic" width="250" height="250">
+  <h1 >Akshay Shah</h1>
+
+</div>
+
+<div class="container-fluid bg-2 text-center">
+  <a href="#DOB" class="btn-info" data-toggle="collapse"><h3 class="margin">born on </h3></a>
+  <h1 id ="DOB" class="margin collapse">18 Jan 1993 </h1>
+  <a href="#PLACE" class="btn-info" data-toggle="collapse"><h3 class="margin">lives at </h3></a>
+  <h1 id="PLACE" class="margin collapse">Pune India.</h1>
+  <p>This is my Digital Profile</p>
+
+</div>
+
+
+
+
+     </div>
+</body>
+</html>
+
