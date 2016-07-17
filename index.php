45a1ad3cffb5e75318a4426e47c896c05feb9945
akshayshah18193 <akshayshah.1893@gmail.com>
2016-07-18 01:26:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-    <link rel="stylesheet" href="main.css" />
+    <link rel="stylesheet" href="main.css"/>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
@@ -15,7 +15,7 @@
 
      <div  class="container-fluid bg-1 text-center">
   <h1 class="margin">Akshay Krishnakumar Shah</h1>
-  <img src="akshay.jpg" class="img-responsive img-circle margin" style="display:inline" alt="Profile Pic" width="250" height="250">
+  <img src="akshay.jpg" class="img-responsive img-circle margin" style="display:inline" alt="myprofilepic" width="250" height="250">
   <h3>and this is my Digital Resume!</h3>
 
 </div>
