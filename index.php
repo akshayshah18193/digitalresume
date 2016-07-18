e5f7ddac2639a248d448de468d891181056617ef
akshayshah18193 <akshayshah.1893@gmail.com>
2016-07-18 17:17:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
   <a href="#PLACE" class="btn-info"  data-toggle="collapse"><h3 class="margin">Lives in </h3></a>
   <div id="PLACE" class="collapse "><h1 class="margin"><span class="glyphicon glyphicon-home"><h2>Pune</h2></span></h1></div>
   <a href="#QUALF" class="btn-info" data-toggle="collapse"><h3 class="margin">Education</h3></a>
-  <div id="QUALF" class="collapse"><h1 class="margin"><span class="glyphicon .glyphicon-education"><h2>BSc. Computer Science</h2></span></h1></div>
+  <div id="QUALF" class="collapse"><h1 class="margin"><span class="glyphicon glyphicon-education"><h2>BSc. Computer Science</h2></span></h1></div>
   <p>This is my Digital Profile</p>
 
 </div>
